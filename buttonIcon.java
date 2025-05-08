362d8ea31287f0e681a04d5af3a8239bd79019a7
francescoColombo16 <francescocolombo2007@gmail.com>
2025-05-08 15:42:44 +0200

    Update buttonIcon.java

--- buttonIcon.java
+++ buttonIcon.java
@@ -6,8 +6,8 @@ import javax.swing.JButton;
 //classe per aggiungere l'immagine su un bottone
 
 public class buttonIcon extends JButton{
-    buttonIcon(String imgPath){
-        setIcon(new ImageIcon(new ImageIcon(imgPath).getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+    buttonIcon(String imgPath, int width, int heigth){
+        setIcon(new ImageIcon(new ImageIcon(imgPath).getImage().getScaledInstance(width, heigtj, Image.SCALE_SMOOTH)));
 
         setBackground(Color.white);
         setBorder(null);
